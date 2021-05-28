ef62e1f3ea06f6a90bb23a111a7d66b8bc7994ec
Dorian Zedler <dorian@itsblue.de>
2021-05-28 10:24:11 +0200

    Add disclaimer to deploy branch

--- createStaticDirectoryListing.py
+++ createStaticDirectoryListing.py
@@ -11,7 +11,7 @@ DEFAULT_TEMPLATE = {
         "FILE": "🗒", #":spiral_notepad:",
         "UP": "⤴", #":arrow_heading_up:"
     },
-    "excludedFiles": ["index.md", "_config.yml", "_layouts"],
+    "excludedFiles": ["index.md", "_config.yml", "_layouts", "README.md"],
     "template": r"""
 # Index of ${path}
 Files in this directory:
